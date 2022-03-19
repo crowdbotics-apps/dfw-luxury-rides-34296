b891e2e0d6cc5cefe6a77add0f23ea0b1daff2f1
Crowdbotics <team@crowdbotics.com>
2022-03-19 06:31:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "dfw_luxury_rides_34296",
+  url: "https://dfw_luxury_rides_34296.botics.co",
+  api: "https://dfw_luxury_rides_34296.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
